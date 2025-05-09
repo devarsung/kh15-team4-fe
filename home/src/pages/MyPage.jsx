f84d13b2782612de4c7a07d9775dd68d007b15b5
devarsung <devarsung@gmail.com>
2025-05-09 17:32:20 +0900

    mypage 화면 구성중

--- home/src/pages/MyPage.jsx
+++ home/src/pages/MyPage.jsx
@@ -1,53 +1,52 @@
 import { useCallback, useEffect, useState } from "react";
-import {Link, useNavigate, useParams} from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { loginState, userLoadingState } from "../utils/storage";
+import { loginState, userLoadingState, userNoState } from "../utils/storage";
 import { useRecoilValue } from "recoil";
 
-export default function MyPage(){
+export default function MyPage() {
     const navigate = useNavigate();
+    const [account, setAccount] = useState({});
 
-    const [boardList, setBoardList] = useState([]);
-    const [title, setTitle] = useState("");
-
+    const userNo = useRecoilValue(userNoState);
     const userLoading = useRecoilValue(userLoadingState);
     const isLogin = useRecoilValue(loginState);
-    useEffect(()=>{
-        if(userLoading === false) return;
-        if(isLogin === false) return;
+    useEffect(() => {
+        if (userLoading === false) return;
+        if (isLogin === false) return;
+        loadAccount();a
+    }, [userLoading, isLogin]);
 
-        loadBoardList();
-    },[userLoading, isLogin]);
+    const loadAccount = useCallback(async () => {
+        const { data } = await axios.get(`/account/${userNo}`);
+        setAccount(data);
+    }, []);
 
-    const createBoard = useCallback(async()=>{
-        await axios.post(`/board/`, {boardTitle: title}).then(resp=>{
-            
-            navigate(`/board/${resp.data}`);
-        });
-    },[title]);
+    const asdf = useCallback(async (e)=>{
 
-    const loadBoardList = useCallback(async ()=>{
-        const {data} = await axios.get(`/board/`);
-        setBoardList(data);
     },[]);
 
-    return(<>
-        <h1>마이페이지</h1>
+    return (<>
+        <div className="container">
+            <div className="mt-4">
+                <h2>My Info</h2>
+            </div>
 
-        <input type="text" value={title} onChange={e=>setTitle(e.target.value)}/>
-        <button onClick={createBoard}>+board</button>
-
-        <div className="row mt-4">
-            {boardList.map(board=>(
-                <div className="col-3" key={board.boardNo}>
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">{board.boardTitle}</h5>
-                            <Link to={`/board/${board.boardNo}`} className="card-link">상세페이지로</Link>
-                        </div>
+            <div className="p-3 border rounded mt-4">
+                <h4 className="mb-3">My BoardList</h4>
+                <div className="row mt-4">
+                    <div className="col-3">닉네임</div>
+                    <div className="col-9">
+                        <input type="text" name="accountNickname" className="form-control" value={account.accountNickname}
+                           readOnly />
                     </div>
                 </div>
-            ))}
+            </div>
+
+
         </div>
+
+
+
     </>)
 }
