dd29d91e1e8f91126c26e5d60c79f5daabb4bc5f
devarsung <devarsung@gmail.com>
2025-05-18 17:58:27 +0900

    접속사 ui

--- home/src/components/BoardHeader.jsx
+++ home/src/components/BoardHeader.jsx
@@ -11,7 +11,7 @@ import { userAccessTokenState, userNoState } from "../utils/storage.js";
 import { useRecoilValue } from "recoil";
 
 export default function BoardHeader(props) {
-    const {boardNo, board} = props;
+    const { boardNo, board } = props;
     const [userList, setUserList] = useState([]);
     const { isOpen, openModal, closeModal } = useModal();
     const userNo = useRecoilValue(userNoState);
@@ -55,11 +55,11 @@ export default function BoardHeader(props) {
 
     const [editMode, setEditMode] = useState(false);
     const [title, setTitle] = useState(board.boardTitle);
-    const changeBoardTitle = useCallback(async()=>{
-        if(title.length <= 0) return;
-        if(title === board.boardTitle) return;
-        await axios.patch(`/board/title/${boardNo}`, {boardTitle: title});
-    },[boardNo, title]);
+    const changeBoardTitle = useCallback(async () => {
+        if (title.length <= 0) return;
+        if (title === board.boardTitle) return;
+        await axios.patch(`/board/title/${boardNo}`, { boardTitle: title });
+    }, [boardNo, title]);
 
     return (<>
         <div className="container-fluid py-3 px-4 bg-white border-bottom">
@@ -67,16 +67,16 @@ export default function BoardHeader(props) {
                 <div className="d-flex align-items-center flex-wrap flex-direction-row">
                     {editMode ? (
                         <input type="text" className="form-control w-auto" autoFocus
-                            value={title} 
-                            onChange={e=>setTitle(e.target.value)}
-                            onBlur={e=>{changeBoardTitle(); setTitle(board.boardTitle); setEditMode(false);}}
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                            onBlur={e => { changeBoardTitle(); setTitle(board.boardTitle); setEditMode(false); }}
                         />
                     ) : (
                         <h3 className="mb-0 me-2 fw-semibold text-dark">{board.boardTitle}</h3>
                     )}
                     {board.accountNo === userNo && (
                         <button className="btn btn-sm btn-outline-secondary" title="제목 수정"
-                            onClick={e=>setEditMode(true)}>
+                            onClick={e => setEditMode(true)}>
                             <BsFillPencilFill />
                         </button>
                     )}
@@ -87,22 +87,26 @@ export default function BoardHeader(props) {
                         <div className="d-flex align-items-center">
                             {userList.length > 0 && (
                                 userList.map(user => (
-                                    <div key={user.accountNo} data-bs-toggle="tooltip" title={user.accountNickname}>
-                                        <Avatar nickname={user.accountNickname} size={36}
-                                            classes={`border me-1 d-none d-sm-flex`} />
+                                    <div key={user.accountNo} data-bs-toggle="tooltip" data-bs-html="true" title={`${user.accountNickname}<br>${user.accountEmail}`}>
+                                        <Avatar nickname={user.accountNickname} size={36} classes={`border me-1 d-none d-sm-flex`} />
                                     </div>
                                 ))
                             )}
                             <button
-                                className="dropdown-toggle rounded-circle bg-light border text-dark d-flex align-items-center justify-content-center fw-bold me-1"
+                                className="d-block d-sm-none dropdown-toggle rounded-circle bg-light border text-dark d-flex align-items-center justify-content-center fw-bold me-1"
                                 style={{ width: "36px", height: "36px" }} title="접속 중인 멤버 보기" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                +4
+                                {userList.length}
                             </button>
                             <ul className="dropdown-menu dropdown-menu-end" style={{ minWidth: "200px" }}>
-                                <li className="dropdown-item d-flex align-items-center">
-                                    <div className="rounded-circle border me-2" style={{ width: "32px", height: "32px", backgroundImage: `url('https://mdbcdn.b-cdn.net/img/new/avatars/1.webp')`, backgroundSize: "cover" }}></div>
-                                    <span>홍길동</span><small className="d-block text-muted">hong@example.com</small>
-                                </li>
+                                {userList.map(user => (
+                                    <li className="dropdown-item d-flex align-items-center" key={user.accountNo}>
+                                        <Avatar nickname={user.accountNickname} size={32} />
+                                        <div className="ms-2">
+                                            <div>{user.accountNickname}</div>
+                                            <small className="d-block text-muted">{user.accountEmail}</small>
+                                        </div>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                     </div>
@@ -114,7 +118,7 @@ export default function BoardHeader(props) {
                             <span>초대</span>
                         </button>
                     )}
-                    
+
 
                     <div className="dropdown">
                         <button className="btn btn-outline-secondary btn-sm" type="button" data-bs-toggle="dropdown">
